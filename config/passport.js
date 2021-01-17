858f6403285927de730f613190f57c51885342a3
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-17 13:05:36 +0300

    auth | passport - define a passport config

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+// Houses jwt strategy config
+// @see - http://www.passportjs.org/packages/passport-jwt/
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../models/User");
+const passportSecret = process.env.JWT_SECRET;
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = passportSecret;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      // get user
+      User.findByPk(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        })
+        .catch((err) =>
+          console.error(
+            "**** passport config: error fetching user by id *****",
+            err
+          )
+        );
+    })
+  );
+};
