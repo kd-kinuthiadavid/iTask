4c90c47b06241db82356917fdf2dd26d61fcb1de
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-17 22:02:20 +0300

    config - set up a navigation router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import { Button } from "reactstrap";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./General/Navbar/index";
 
 function App() {
   return (
-    <div className="App">
-      client
-      <Button outline color="primary">
-        primary
-      </Button>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Button outline color="primary">
+            primary
+          </Button>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
