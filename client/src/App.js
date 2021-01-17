e215cc2646c854c599cd6e8b87755be6a9634ec6
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-18 01:14:24 +0300

    redux - user <Provider /> to pass store to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
@@ -6,15 +8,17 @@ import Navbar from "./General/Navbar";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
