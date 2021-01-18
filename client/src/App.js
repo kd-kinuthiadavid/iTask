4c25a8292bf6a974fdd77f3a9938deebb510b31a
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-18 03:20:22 +0300

    auth - check whether a user is authenticated when the app is mounted
    persists user info

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
+import { useEffect } from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import jwt_decode from "jwt-decode";
 
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import Navbar from "./General/Navbar";
 import Dashboard from "./Dashboard";
+import setAuthTokenHeader from "./utils/setAuthTokenHeader";
+import * as types from "./redux/actionTypes";
 
 function App() {
+  useEffect(() => {
+    // check if user is logged in
+    if (localStorage.getItem("jwtToken")) {
+      // set auth header
+      setAuthTokenHeader(localStorage.getItem("jwtToken"));
+      // get user info
+      const decodedJwtToken = jwt_decode(localStorage.getItem("jwtToken"));
+
+      // set current user
+      store.dispatch({
+        type: types.SET_CURRENT_USER,
+        payload: decodedJwtToken,
+      });
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
